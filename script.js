9e20570645eee9c3fe14e5a58a8941b82a197fc4
juanframart2011 <juanframart2011@gmail.com>
2025-02-12 22:28:42 -0600

    ajuste de server

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 $('document').ready(function() {
     $('#formulario').on('submit', function(e) {
         e.preventDefault();
-        var tarea = $('<li/>');
-
-        tarea.html('<span>' + $('#nueva-tarea').val() + '</span>');
-
-        tarea.addClass('list-group-item');
-
-        tarea.on('click', function() {
-            $(this).append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).off('click');
-        });
-
-        $('#mi-lista-de-tareas').append(tarea);
+        
+        creaTarea({
+            title: $('#tarea').val(),
+            completed: false
+        })
     });
+
+    function renderTareas(items){
+
+        items.forEach(function(tarea) {
+            var tareaElement = $('<li/>');
+            tareaElement.html('<span>' + tarea.title + '</span>');
+            tareaElement.addClass('list-group-item');
+            tareaElement.on('click', function() {
+                $(this).append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).off('click');
+            });
+            $('#mi-lista-de-tareas').append(tareaElement);
+        });
+    }
+
+    function traerTareas(){
+        $.ajax({
+            url: 'http://localhost:3000/tareas',
+            method: 'GET',
+        }).then(function(response) {
+            renderTareas(response);
+        });
+    }
+
+    traerTareas();
+
+    function creaTarea( item ){
+        $.ajax({
+            url: 'http://localhost:3000/tareas',
+            method: 'POST',
+            data: item
+        }).then(function(item) {
+            renderTareas(item);
+        });
+    }
 });
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-    res.status(200).json([{
-        key: 'value'
-    }]);
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+var tareas = [{
+    _id: 1,
+    title: 'Tarea 1',
+    completed: false
+}, {
+    _id: 2,
+    title: 'Tarea 2',
+    completed: false
+}, {
+    _id: 3,
+    title: 'Tarea 3',
+    completed: false
+}];
+
+app.get('/tareas', function(req, res) {
+    res.status(200).json(tareas);
+});
+
+app.post('/tareas', function(req, res) {
+    let nuevaTarea = req.body;
+    nuevaTarea._id = tareas.length + 1;
+    tareas.push(nuevaTarea);
+    res.status(200).json(nuevaTarea);
 });
 
 app.listen(3000, function() {
