4dffb204dfd250fd76fae948433acf242f1cef52
juanframart2011 <juanframart2011@gmail.com>
2025-02-16 21:31:58 -0600

    finis course

--- script.js
+++ script.js
@@ -1,44 +1,85 @@
 $('document').ready(function() {
     $('#formulario').on('submit', function(e) {
         e.preventDefault();
-        
-        creaTarea({
-            title: $('#tarea').val(),
-            completed: false
-        })
+        crearTarea({
+            title: $('#nueva-tarea').val()
+        });
     });
 
-    function renderTareas(items){
+    function dibujarTareas(items) {
+        items.forEach(function(item) {
+            var tarea = $('<li/>');
 
-        items.forEach(function(tarea) {
-            var tareaElement = $('<li/>');
-            tareaElement.html('<span>' + tarea.title + '</span>');
-            tareaElement.addClass('list-group-item');
-            tareaElement.on('click', function() {
-                $(this).append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).off('click');
-            });
-            $('#mi-lista-de-tareas').append(tareaElement);
+            tarea.attr('id', item._id);
+
+            tarea.html('<span>' + item.title + '</span>');
+
+            tarea.addClass('list-group-item');
+
+            if (item.completed) {
+                tarea.append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).on('click', function() {
+                    var self = $(this);
+                    eliminarTarea(self.attr('id'), function() {
+                        self.remove();
+                    });
+                });
+            } else {
+                tarea.on('click', function() {
+                    var self = $(this);
+                    completarTarea(self.attr('id'), function() {
+                        self.append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).off('click');
+                    });
+                });
+
+                $('#mi-lista-de-tareas').append(tarea);
+            }
         });
     }
 
-    function traerTareas(){
+    function traerTareas() {
         $.ajax({
             url: 'http://localhost:3000/tareas',
-            method: 'GET',
-        }).then(function(response) {
-            renderTareas(response);
+            method: 'get'
+        }).then(function(items) {
+            dibujarTareas(items);
         });
     }
 
     traerTareas();
 
-    function creaTarea( item ){
+    function crearTarea(item) {
         $.ajax({
             url: 'http://localhost:3000/tareas',
-            method: 'POST',
+            method: 'post',
             data: item
         }).then(function(item) {
-            renderTareas(item);
+            var tarea = $('<li/>');
+
+            tarea.attr('id', item._id);
+
+            tarea.html('<span>' + item.title + '</span>');
+
+            tarea.addClass('list-group-item');
+
+            tarea.on('click', function() {
+                $(this).append('<b style="float:right">✓</b>').appendTo($('#mi-lista-de-tareas-completadas')).off('click');
+            });
+
+            $('#mi-lista-de-tareas').append(tarea);
         });
+    }
+
+    function completarTarea(id, cb) {
+        $.ajax({
+            url: 'http://localhost:3000/tareas/' + id + '/completar',
+            method: 'put'
+        }).then(cb);
+    }
+
+    function eliminarTarea(id, cb) {
+        $.ajax({
+            url: 'http://localhost:3000/tareas/' + id,
+            method: 'delete'
+        }).then(cb);
     }
 });
--- server.js
+++ server.js
@@ -1,39 +1,62 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var db = require('mongoose');
+var TareaModel = require('./tarea');
 
-app.all('*', function(req, res, next) {
+var connectDb = function() {
+    db.connect('mongodb://localhost:27017/curso-antes-de-iniciarte-en-el-desarrollo-web-lista-de-tareas', {
+        promiseLibrary: global.Promise
+    }).then(function() {
+        // console.log('db-connect', DB_URI);
+    }, function(err) {
+        // console.log('err-db-connect', err);
+        connectDb();
+    });
+};
+
+db.Promise = global.Promise;
+connectDb();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.all('*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, X-UserId, X-Nonce' +
+        ', X-Secret, X-Ts, X-Sig, X-Vendor-Sig, X-Vendor-Apikey, X-Vendor-Nonce, X-Vendor-Ts, X-ProfileId' +
+        ', X-Authorization, Authorization, Token, Pragma, Cache-Control, Expires');
+    res.header('Access-Control-Allow-Methods', 'HEAD,OPTIONS,GET,PUT,POST,DELETE');
     next();
 });
 
-var tareas = [{
-    _id: 1,
-    title: 'Tarea 1',
-    completed: false
-}, {
-    _id: 2,
-    title: 'Tarea 2',
-    completed: false
-}, {
-    _id: 3,
-    title: 'Tarea 3',
-    completed: false
-}];
-
 app.get('/tareas', function(req, res) {
-    res.status(200).json(tareas);
+    TareaModel.find().lean().exec(function(err, tareas) {
+        res.status(200).json(tareas);
+    });
 });
 
 app.post('/tareas', function(req, res) {
-    let nuevaTarea = req.body;
-    nuevaTarea._id = tareas.length + 1;
-    tareas.push(nuevaTarea);
-    res.status(200).json(nuevaTarea);
+    TareaModel.create(req.body, function(err, nuevaTarea) {
+        res.status(200).json(nuevaTarea);
+    });
+});
+
+app.put('/tareas/:tareaId/completar', function(req, res) {
+    TareaModel.findByIdAndUpdate(req.params.tareaId, {
+        completed: true
+    }).lean().exec(function(err, tarea) {
+        res.status(200).json(tarea);
+    });
+});
+
+app.delete('/tareas/:tareaId', function(req, res) {
+    TareaModel.findByIdAndRemove(req.params.tareaId).lean().exec(function(err, tarea) {
+        res.status(200).json(tarea);
+    });
 });
 
 app.listen(3000, function() {
